24326230254831a8ebc4d42b018e2578332324b0
plausibility <chris@gibsonsec.org>
2013-01-17 23:24:35 +1100

    Oh yes, py26 support, since it doesn't break.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def long_desc():
 
 kw = {
     "name": "pysqlw",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "description": "Python wrapper to make MySQL and SQLite easy",
     "long_description": long_desc(),
     "url": "https://github.com/plausibility/pysqlw",
